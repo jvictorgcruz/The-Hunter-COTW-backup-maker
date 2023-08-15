f9b8f12fa19a796062847f52069faa358f98ba84
João Victor Garcia <jvictorgcruz@gmail.com>
2023-08-15 12:07:11 -0300

    fix: rebase comment lines and create dir verify

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from datetime import datetime
 
 
 def create_zip(directory_path):
-    # Verificar se o diretório existe
-    if not os.path.exists(directory_path):
-        print(f"Diretório '{directory_path}' não existe.")
-        return
 
-    # Obter o nome base do diretório
+    # Verify if dir exists
+    if not os.path.exists(directory_path):
+       raise Exception(f"folder '{directory_path}' doesnt exists.")
+
+    # Get dir basename
     base_name = os.path.basename(directory_path)
     folder_path = os.path.dirname(directory_path)
     zip_path = os.path.join(folder_path, f'{base_name}.zip')
 
-    # Criar arquivo ZIP a partir do diretório
+    # Create zip by dir
     with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, _, files in os.walk(directory_path):
             for file in _:
@@ -28,22 +28,28 @@ def create_zip(directory_path):
                 zipf.write(file_path, arcname)
 
 def remove_directory(directory_path):
-    # Excluir o diretório original
+    # Delete copy dir
     shutil.rmtree(directory_path)
 
 
 def copy_and_zip(source_folder, backup_folder):
-    # Criar pasta de backup se não existir
+
+    # Verify if game save folder exists
+    if not os.path.exists(source_folder):
+        raise Exception(f'Folder "{source_folder}" doesnt exists')
+    
+    # Create backup folder if not yet exists
     if not os.path.exists(backup_folder):
         os.makedirs(backup_folder)
 
+    # Create backup filename with timestamp
     now = datetime.now()
     timestamp = now.strftime('%Y-%m-%d_%H-%M-%S')
     backup_filename = f'bkp_{timestamp}'
-
+    
     backup_path = os.path.join(source_folder, backup_folder, backup_filename)
 
-    # Copiar pastas de origem para a pasta de backup
+    # Create folder copy to backup_folder
     for item in os.listdir(source_folder):
         item_path = os.path.join(source_folder, item)
         if os.path.isdir(item_path) and item != backup_folder:
@@ -53,7 +59,7 @@ def copy_and_zip(source_folder, backup_folder):
     create_zip(backup_path)
     remove_directory(backup_path)
 
-    print('Backup criado e compactado com sucesso.')
+    print('Backup created with sucess')
 
 source_folder = os.path.join(os.path.expanduser('~'), 'Documents', 'Avalanche Studios')
 backup_folder = 'Bkps'
